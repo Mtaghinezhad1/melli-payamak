03cfe563137d956e762c0da80926e6d201111566
Mtaghinezhad1 <mtaghinezhad1@gmail.com>
2023-09-02 14:09:40 +0330

    header created

---
+++ __import/header/title/phone.php
@@ -0,0 +1,32 @@
+<div class="phone">
+    <div class="border border-1 rounded-2 w-100 h-100">
+
+        <!-- phone header -->
+        <div class="mg-h100rem d-flex flex-column justify-content-between align-items-between border-bottom p-2">
+            <div class="d-flex align-items-center justify-content-between">
+                <div style="background-color: var(--pa-bg-green-100);" class="mg-w44rem mg-h8rem rounded-pill"></div>
+                <div style="background-color: var(--pa-bg-green-100);" class="mg-w44rem mg-h8rem rounded-pill"></div>
+                <div style="background-color: var(--pa-bg-green-100);" class="mg-w44rem mg-h8rem rounded-pill"></div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+                <div style="background-color: var(--pa-bg-green-100); font-size: 0.7rem" class="mg-w20rem mg-h20rem d-flex align-items-center justify-content-center rounded-circle">
+                    i
+                </div>
+                <div id="middle-circle" style="background-color: var(--pa-bg-green-100); font-size: 0.7rem" class="mg-w28rem mg-h28rem d-flex align-items-center justify-content-center rounded-circle">M</div>
+                <div style="background-color: var(--pa-bg-green-100); font-size: 0.7rem" class="mg-w20rem mg-h20rem d-flex align-items-center justify-content-center rounded-circle">
+                    ۴
+                </div>
+            </div>
+            <div id="text-phone-header" style="font-size: 0.9rem" class="text-center">نساجی بروجرد</div>
+        </div>
+
+        <!-- phone body -->
+        <div class="p-2">
+            <div id="text-phone-body" style="background-color: var(--pa-bg-green-300); font-size: 0.9rem" class="rounded-3 px-2 py-3 text-white lh-lg">
+                جناب آقاي زين العابدين نجار صادقي از خريد شما متشكريم.
+                چرم مشهد تلفن امور مشتركين:
+                ۰۲۱-۲۳۴۹۷۷۹
+            </div>
+        </div>
+    </div>
+</div>
--- template/js/index.js
+++ template/js/index.js
@@ -3,6 +3,28 @@ const topAdsBtn = document.getElementById('topAdsBtn');
 const menuBtn = document.getElementById('menu-sm-btn');
 const dropdownContainer = document.getElementById('dropDownContainer');
 const dropTriggers = document.querySelectorAll(".dropTrigger");
+const middleCircle = document.querySelector('#middle-circle');
+const textPhoneHeader = document.querySelector('#text-phone-header');
+const textPhoneBody = document.querySelector('#text-phone-body');
+let counter = 0;
+
+// data for phone messages
+const data = [
+    {
+        letter: 'S',
+        messageName: 'سان استار',
+        messageText: 'پيج رسمي سان استار در ايران\n' +
+            'instagram.com/sunstariran\n' +
+            '+Follow Us'
+    },
+    {
+        letter: 'M',
+        messageName: 'چرم مشهد',
+        messageText: 'جناب آقاي زين العابدين نجار صادقي از خريد شما متشكريم.\n' +
+            'چرم مشهد تلفن امور مشتركين:\n' +
+            '021-22012xxx'
+    }
+]
 
 // event listener for topAds
 topAdsBtn.addEventListener("click", ()=>{
@@ -20,8 +42,38 @@ dropTriggers.forEach(el => {
     el.addEventListener('mouseover',()=>{
         dropdownContainer.style.left= `${xRight}px`;
     })
-    console.log(xRight);
 })
+
+// animating the phone content
+setInterval(()=>{
+    middleCircle.classList.add('fadeOut');
+    textPhoneHeader.classList.add('fadeOut');
+    textPhoneBody.classList.add('fadeOut');
+    setTimeout(()=>{
+        if (counter < data.length){
+            middleCircle.innerHTML = data[counter].letter;
+            textPhoneHeader.innerHTML = data[counter].messageName;
+            textPhoneBody.innerHTML = data[counter].messageText;
+            counter++;
+        }else {
+            middleCircle.innerHTML = data[0].letter;
+            textPhoneHeader.innerHTML = data[0].messageName;
+            textPhoneBody.innerHTML = data[0].messageText;
+            counter = 1;
+        }
+        middleCircle.classList.remove('fadeOut');
+        textPhoneHeader.classList.remove('fadeOut');
+        textPhoneBody.classList.remove('fadeOut');
+        middleCircle.classList.add('fadeIn');
+        textPhoneHeader.classList.add('fadeIn');
+        textPhoneBody.classList.add('fadeIn');
+    },200);
+    setTimeout(()=>{
+        middleCircle.classList.remove('fadeIn');
+        textPhoneHeader.classList.remove('fadeIn');
+        textPhoneBody.classList.remove('fadeIn');
+    },400);
+},3000)
 
 
 
